50efc26bafcf86035cda5e665dd53c27493d900c
Haoli Yin <haoliyin2002@gmail.com>
2021-11-01 10:41:16 -0500

    update repo

---
+++ main.py
@@ -0,0 +1,7 @@
+
+def current_price(): #input opensea link or api call results
+    #compare last sold price and current lowest listing price and take the min of both values
+    #return min
+
+if __name__ == "__main__":
+    print("Hello World")
